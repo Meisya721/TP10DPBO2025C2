ad3d0db4944f6887f6bc50b06b00dc4241f71747
Meisya721 <meilia.23@upi.edu>
2025-05-23 20:33:35 +0700

    Add files via upload

---
+++ TP10_DPBO/config/Database.php
@@ -0,0 +1,20 @@
+<?php
+class Database {
+    private $host = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $database = 'makeup';
+    private $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->database, $this->username, $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {echo "Connection error: " . $exception->getMessage();}
+        return $this->conn;
+    }
+}
+?>
